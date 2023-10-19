3bc4e78b8ebce8b32638082be0d9aa59c0f90cad
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-10-19 17:36:58 +0300

    wip

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
+
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <span>Arturs Langenfelds</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          Arturs Langenfelds
+        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="facebook" />
